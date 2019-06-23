9dc2a67047913a80156e79035538449604ec311e
Luca <luca.cavagnoli@gmail.com>
2019-06-23 18:26:39 +0200

    doc

--- src/test/java/lucaster/poc/ddd/jpa/v1/domain/ExpView.java
+++ src/test/java/lucaster/poc/ddd/jpa/v1/domain/ExpView.java
@@ -1,6 +1,7 @@
 package lucaster.poc.ddd.jpa.v1.domain;
 
-// TODO: maybe Domain interfaces are redundant and only add complexity..?
+// TODO: maybe Domain interfaces are redundant and only add complexity..? 
+// ...Or they help connecting the Anemic Jpa classes to Domain concemps?
 public interface ExpView<T extends ExpViewChild>
         extends ExpViewIndependentFields, ExpViewDerivedFields, HasExpViewChildren<T> {
 
