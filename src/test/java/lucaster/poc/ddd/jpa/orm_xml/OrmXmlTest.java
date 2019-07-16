5230c12bebef079751c00af6c2f223ece67d681a
Luca <luca.cavagnoli@gmail.com>
2019-07-16 10:47:02 +0200

    preparation

---
+++ src/test/java/lucaster/poc/ddd/jpa/orm_xml/OrmXmlTest.java
@@ -0,0 +1,32 @@
+package lucaster.poc.ddd.jpa.orm_xml;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+public class OrmXmlTest {
+
+    @Test
+    public void canCreateEm() {
+        // Arrange
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("testOrmXml");
+        
+        // Act
+        EntityManager em = emf.createEntityManager();
+
+        // Assert
+    }
+
+    @Test
+    public void canPersistDomainEntity() {
+        // Arrange
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("testOrmXml");
+        EntityManager em = emf.createEntityManager();g
+        
+        // Act
+
+        // Assert
+    }
+}
