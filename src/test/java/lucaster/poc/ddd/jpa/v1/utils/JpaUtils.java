79bc610fea7364dbf5fd1e94adacc805bbf96fff
Luca <luca.cavagnoli@gmail.com>
2019-07-16 10:46:32 +0200

    Jpa Utils

--- src/test/java/lucaster/poc/ddd/jpa/v1/utils/JpaUtils.java
+++ src/test/java/lucaster/poc/ddd/jpa/v1/utils/JpaUtils.java
@@ -9,12 +9,31 @@ import javax.persistence.Persistence;
 
 public final class JpaUtils {
 
+	private JpaUtils() {}
+
 	@SafeVarargs
 	public static <T> T commitInJpa(Function<EntityManager, ? extends Object>... fns) {
+		String persistenceUnitName = "test";
+		@SuppressWarnings("unchecked")
+		T cast = (T) getCommitterInJpa(persistenceUnitName).apply(fns);
+		return cast;
+	}
 
+	public static <T> Function<Function<EntityManager, ? extends Object>[], T> getCommitterInJpa(final String persistenceUnitName) {
+		return new Function<Function<EntityManager, ? extends Object>[], T>() {
+
+			@Override
+			public T apply(Function<EntityManager, ? extends Object>[] fns) {
+				return commitInJpa(persistenceUnitName, fns);
+			}
+		};
+	}
+
+	@SafeVarargs
+	public static <T> T commitInJpa(String persistenceUnitName, Function<EntityManager, ? extends Object>... fns) {
 		Object result = null;
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
 
