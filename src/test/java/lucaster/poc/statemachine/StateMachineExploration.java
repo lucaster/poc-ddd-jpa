8cfbfc4a0f1372fd0561caa0f1952d2a6a67cb5b
Luca <luca.cavagnoli@gmail.com>
2019-08-01 09:31:06 +0200

    LoD

--- src/test/java/lucaster/poc/statemachine/StateMachineExploration.java
+++ src/test/java/lucaster/poc/statemachine/StateMachineExploration.java
@@ -3,23 +3,16 @@ package lucaster.poc.statemachine;
 import java.util.Map;
 import java.util.Set;
 
-interface ProcessDefinitionRepository {
-    ProcessDefinition findProcessDefinition(String processDefinitionName, String processDefinitionVersion);
-}
 abstract class ProcessDefinition {
     abstract public String getName();
     abstract public String getVersion();
-    abstract protected ProcessInstance newProcessInstance();
 }
-interface ProcessInstanceFactory {
-    ProcessInstance newProcessInstance(ProcessDefinition processDefinition);
-}
-interface ProcessInstanceRepository {
-    ProcessInstance findProcessInstance(String domainEntityIdentifier);
+interface ProcessDefinitionRepository {
+    ProcessDefinition findProcessDefinition(String processDefinitionName, String processDefinitionVersion);
 }
 abstract class ProcessInstance {
     /**
-     * E.G. Checlkist
+     * Identifier of the Domain Entity this Process Instance manages the workflow of.
      */
     abstract public String getDomainEntityIdentifier();
     abstract public boolean isTerminated();
@@ -31,7 +24,7 @@ abstract class ProcessInstance {
     abstract public boolean canActivate(String roleName, String transitionName);    
     abstract public String activate(String roleName, String transitionName) throws IllegalStateException;
 
-    // If returned objects are immutable and do not modify anything, these methods can become public and compromise with LoD:
+    // If returned objects are immutable and do not modify anything, these type-safe methods can become public and compromise with LoD:
     abstract protected Map<Role, Set<Transition>> getAllowedActivationsTypesafe();
     abstract protected boolean canActivate(Role role, Transition transition);    
     abstract protected State activate(Role role, Transition transition) throws IllegalStateException;
@@ -40,6 +33,16 @@ abstract class ProcessInstance {
     abstract protected State getActiveState();
     abstract protected Set<Transition> getActiveTransitions();
     abstract protected Transition findTransitionByName(String transitionName);
+
+    abstract protected Set<Transition> getTransitions();
+    abstract protected Set<State> getStates();
+    abstract protected Set<Role> getRoles();
+}
+interface ProcessInstanceFactory {
+    ProcessInstance newProcessInstance(ProcessDefinition processDefinition);
+}
+interface ProcessInstanceRepository {
+    ProcessInstance findProcessInstance(String domainEntityIdentifier);
 }
 abstract class State {
     // Unique within ProcessDefinition
