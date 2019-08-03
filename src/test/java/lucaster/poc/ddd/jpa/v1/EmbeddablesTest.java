193df79a473e8b007483958f8a09a01b8c644309
Luca <luca.cavagnoli@gmail.com>
2019-08-03 11:10:19 +0200

    explanation

--- src/test/java/lucaster/poc/ddd/jpa/v1/EmbeddablesTest.java
+++ src/test/java/lucaster/poc/ddd/jpa/v1/EmbeddablesTest.java
@@ -161,6 +161,8 @@ public class EmbeddablesTest {
                     em.persist(cl);
                     em.flush();
 
+                    System.out.println("updatingSourceFieldsTheSecondTime - flush 1");
+
                     clId = cl.getId();
 
                     em.detach(cl.getOverview());
@@ -175,7 +177,10 @@ public class EmbeddablesTest {
                 @Override
                 public Void apply(EntityManager em) {
 
-                    Checklist theCl = em.find(Checklist.class, clId); // also loads weak entities because of https://vladmihalcea.com/the-best-way-to-map-a-onetoone-relationship-with-jpa-and-hibernate/
+                    // Also loads weak entities because of https://vladmihalcea.com/the-best-way-to-map-a-onetoone-relationship-with-jpa-and-hibernate/
+                    // That's because bidirectional @OneToOne loads children entity eagerly when loading the parent anyway.
+                    // This has the (positive) side effect that derived fields of children get updated automatically.
+                    Checklist theCl = em.find(Checklist.class, clId); 
                     Screening theSc = theCl.getScreening();
                     theSc.setScreeningField1(2);
                     theSc.setScreeningField2(4);
