d68eb43bf5e36bf42d1dfe802d9f7ce8b908368e
Luca <luca.cavagnoli@gmail.com>
2019-08-23 07:45:10 +0200

    state machine exploration 2 wip

---
+++ src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2.java
@@ -0,0 +1,84 @@
+package lucaster.poc.statemachine.exploration2;
+
+public class StateMachineExploration2 {
+    void explore() {
+        Usher usher = null;
+        boolean can = usher.canExecute("P123456", "approveProposal", "proposal-id-123-456-789");
+    }
+}
+
+interface Usher {
+    boolean canExecute(String username, String taskId, String appInstanceId);
+}
+
+abstract class AbstractUsher implements Usher {
+    private Query query;
+    public boolean canExecute(String username, String processId, String taskName, String appInstanceId) {
+        return 
+            hasRoleForTask(username, processId, taskName) 
+            && 
+            isActiveTask(taskName, appInstanceId) 
+            && 
+            isTheActivePersonForTaskOfInstance(username, taskName, appInstanceId)
+            ;
+    }
+
+    // Process Definition concern
+    boolean hasRoleForTask(String username, String processId, String taskName) {
+        Iterable<Role> userRoles = query.findRoles(username);
+        ProcessDefinition pd = query.findProcessDefinition(processId);
+        Task task = query.findTask(pd, taskName);
+        Iterable<Role> taskRoles = query.findTaskRoles(task);
+        return anyEquals(userRoles, taskRoles);
+    }
+
+    // Process Instance concern
+    boolean isActiveTask(String taskName, String appInstanceId) {
+        ProcessInstance pi = query.findProcessInstance(appInstanceId);
+        ProcessDefinition pd = query.findProcessDefinition(pi);
+        Task task = query.findTask(pd, taskName);
+        Iterable<Task> activeTasks = query.getActiveTasks(pi);
+        return anyEquals(task, activeTasks);
+    }
+
+    /**
+     * Application - specific
+     */
+    abstract boolean isTheActivePersonForTaskOfInstance(String username, String taskName, String appInstanceId);
+
+    boolean anyEquals(Iterable<Role> roles1, Iterable<Role> roles2) {
+        for (Role role1 : roles1) {
+            for (Role role2 : roles2) {
+                if (role1.equals(role2)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean anyEquals(Task task, Iterable<Task> activeTasks) {
+        for (Task activeTask : activeTasks) {
+            if (activeTask.equals(task)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+interface Query {
+    Iterable<Role> findRoles(String username);
+    ProcessDefinition findProcessDefinition(String processId);
+    Task findTask(ProcessDefinition pd, String taskName);
+    Iterable<Role> findTaskRoles(Task task);
+    ProcessInstance findProcessInstance(String appInstanceId);
+    ProcessDefinition findProcessDefinition(ProcessInstance pi);
+    // If SM, active tasks are the current state's outgoing transitions. If BPMN, active tasks are the active tasks.
+    Iterable<Task> getActiveTasks(ProcessInstance pi);
+}
+
+interface Role {}
+interface ProcessDefinition {}
+interface Task {}
+interface ProcessInstance {}
