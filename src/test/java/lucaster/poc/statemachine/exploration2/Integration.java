e5f79ad0bb1f5237eb16a7c9be81ddbf333f1e57
Luca <luca.cavagnoli@gmail.com>
2019-08-24 21:02:09 +0200

    YAGNI

--- src/test/java/lucaster/poc/statemachine/exploration2/Integration.java
+++ src/test/java/lucaster/poc/statemachine/exploration2/Integration.java
@@ -5,26 +5,12 @@ package lucaster.poc.statemachine.exploration2;
  * @author Luca Cavagnoli
  *
  */
-abstract class AppProcInst {
+class AppProcInst {
     final ProcessInstance pi;
     final String appInstanceId;
-    AppProcInst(ProcessInstance pi, String appInstanceId /* + active state if FSM or active tasks if BPMN */) {
+    protected Long versionForOptimisticLocking;
+    AppProcInst(ProcessInstance pi, String appInstanceId) {
         this.pi = pi;
         this.appInstanceId = appInstanceId;
     }
-}
-
-class AppSmInst extends AppProcInst {
-	private StateMachineState activeState;
-	protected Long versionForOptimisticLocking;
-	AppSmInst(StateMachineInstance pi, String appInstanceId, StateMachineState activeState) {
-		super(pi, appInstanceId);
-		this.setActiveState(activeState);
-	}
-	StateMachineState getActiveState() {
-		return activeState;
-	}
-	void setActiveState(StateMachineState activeState) {
-		this.activeState = activeState;
-	}
 }
