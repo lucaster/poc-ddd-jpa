9888f6c0af2f484714fc9b17b7aa65e264a51632
Luca <luca.cavagnoli@gmail.com>
2019-08-24 23:38:16 +0200

    file extract

--- src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2Api.java
+++ src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2Api.java
@@ -1,9 +1,5 @@
 package lucaster.poc.statemachine.exploration2;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 interface Usher {
     boolean canExecute(String username, String taskName, String appInstanceId);
 }
@@ -101,40 +97,5 @@ final class ModelDrivenUsherProcessTopologyQuery implements UsherProcessTopology
     @Override
     public Iterable<Task> getActiveTasks(ProcessInstance pi) {
         return pi.getActiveTasks();
-    }
-}
-
-
-
-
-
-abstract class Utils {
-    private Utils() {}
-    @SafeVarargs
-    public static <T> Set<T> toSet(T... args) {
-        Set<T> set = new HashSet<>();
-        for (T t : args) {
-            set.add(t);
-        }
-        return Collections.unmodifiableSet(set);
-    }
-    static <T> boolean anyEquals(Iterable<T> roles1, Iterable<T> roles2) {
-        for (T role1 : roles1) {
-            if (anyEquals(role1, roles2)){
-                return true;
-            } 
-        }
-        return false;
-    }
-    static <T> boolean anyEquals(T task1, Iterable<T> tasks2) {
-        for (T activeTask : tasks2) {
-            if (activeTask.equals(task1)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    static String makeFullyQualifiedStateName(String processDefinitionId, String stateName) {
-    	 return String.format("%s|||%s", processDefinitionId, stateName);
     }
 }
