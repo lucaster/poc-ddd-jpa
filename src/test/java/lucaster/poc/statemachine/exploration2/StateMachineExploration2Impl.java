4fdb8705a423a036f79d771eafd71f06e5504d32
Luca <luca.cavagnoli@gmail.com>
2019-08-24 16:30:46 +0200

    extracted to files

---
+++ src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2Impl.java
@@ -0,0 +1,11 @@
+package lucaster.poc.statemachine.exploration2;
+
+class LeverageUsher extends AbstractUsher {
+
+    @Override
+    boolean isTheActivePersonForTaskOfInstance(String username, String taskName, String appInstanceId) {
+        return false;
+    }
+
+   
+}
