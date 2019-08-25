ae17406fa09a16606c1c20163400bcc90ff6d7c3
Luca <luca.cavagnoli@gmail.com>
2019-08-25 13:35:11 +0200

    rename

--- src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2Test.java
+++ src/test/java/lucaster/poc/statemachine/exploration2/StateMachineExploration2Test.java
@@ -21,8 +21,8 @@ public class StateMachineExploration2Test {
 
 	String username1;
 	String username2;
-	String taskName1;
-	String taskName2;
+	String task1Name;
+	String task2Name;
 	String appInstanceId;
 
 	@Before
@@ -39,8 +39,8 @@ public class StateMachineExploration2Test {
 
 		username1 = "EE53414";
 		username2 = "EE37987";
-		taskName1 = ExampleSmProcessTransitions.TASK1.getTaskName();
-		taskName2 = ExampleSmProcessTransitions.TASK2.getTaskName();
+		task1Name = ExampleSmProcessTransitions.TASK1.getTaskName();
+		task2Name = ExampleSmProcessTransitions.TASK2.getTaskName();
 		appInstanceId = "proposalId123";
 	}
 
@@ -50,12 +50,12 @@ public class StateMachineExploration2Test {
 		ProcessDefinition pd = procTopoQuery.findProcessDefinition(pi);
 		String processId = pd.getProcessDefinitionId();
 		ProcessDefinition pd2 = procTopoQuery.findProcessDefinition(processId);
-		Task task = procTopoQuery.findTask(pd, taskName1);
-		Task task2 = procTopoQuery.findTask(pd2, taskName1);
+		Task task = procTopoQuery.findTask(pd, task1Name);
+		Task task2 = procTopoQuery.findTask(pd2, task1Name);
 
 		assertEquals(pd, pd2);
 		assertEquals(task, task2);
-		assertEquals(taskName1, task2.getTaskName());
+		assertEquals(task1Name, task2.getTaskName());
 	}
 
 	@Test
@@ -63,43 +63,43 @@ public class StateMachineExploration2Test {
 		ProcessInstance pi = procIntegrQuery.findProcessInstanceByAppIntanceId(appInstanceId);
 		ProcessDefinition pd = procTopoQuery.findProcessDefinition(pi);
 		String processId = pd.getProcessDefinitionId();
-		boolean hasRoleForTask = ((TestUsherImpl) usher).hasRoleForTask(username1, processId, taskName1);
+		boolean hasRoleForTask = ((TestUsherImpl) usher).hasRoleForTask(username1, processId, task1Name);
 		assertTrue(hasRoleForTask);
 	}
 
 	@Test
 	public void isActiveTask() {
-		boolean isActiveTask = ((TestUsherImpl) usher).isActiveTask(taskName1, appInstanceId);
+		boolean isActiveTask = ((TestUsherImpl) usher).isActiveTask(task1Name, appInstanceId);
 		assertTrue(isActiveTask);
 	}
 
 	@Test
 	public void canExecute() {
-		assertTrue(usher.canExecute(username1, taskName1, appInstanceId));
-		assertFalse(usher.canExecute(username1, taskName2, appInstanceId));
-		assertFalse(usher.canExecute(username2, taskName1, appInstanceId));
-		assertFalse(usher.canExecute(username2, taskName2, appInstanceId));
+		assertTrue(usher.canExecute(username1, task1Name, appInstanceId));
+		assertFalse(usher.canExecute(username1, task2Name, appInstanceId));
+		assertFalse(usher.canExecute(username2, task1Name, appInstanceId));
+		assertFalse(usher.canExecute(username2, task2Name, appInstanceId));
 	}
 
 	@Test
 	public void execute() {		
-		executor.execute(username1, appInstanceId, taskName1);
+		executor.execute(username1, appInstanceId, task1Name);
 		
 		ProcessInstance processInstance = procIntegrQuery.findProcessInstanceByAppIntanceId(appInstanceId);
 		StateMachineInstance stateMachineInstance = (StateMachineInstance) processInstance;
 		StateMachineState activeState = stateMachineInstance.getActiveState();
 		assertEquals(ExampleSmProcessStates.STATE2, activeState);
 		
-		executor.execute(username2, appInstanceId, taskName2);
+		executor.execute(username2, appInstanceId, task2Name);
 		
 		ProcessInstance processInstance2 = procIntegrQuery.findProcessInstanceByAppIntanceId(appInstanceId);
 		StateMachineInstance stateMachineInstance2 = (StateMachineInstance) processInstance2;
 		StateMachineState activeState2 = stateMachineInstance2.getActiveState();
 		assertEquals(ExampleSmProcessStates.STATE3, activeState2);
 		
-		assertFalse(usher.canExecute(username1, taskName1, appInstanceId));
-		assertFalse(usher.canExecute(username1, taskName2, appInstanceId));
-		assertFalse(usher.canExecute(username2, taskName1, appInstanceId));
-		assertFalse(usher.canExecute(username2, taskName2, appInstanceId));
+		assertFalse(usher.canExecute(username1, task1Name, appInstanceId));
+		assertFalse(usher.canExecute(username1, task2Name, appInstanceId));
+		assertFalse(usher.canExecute(username2, task1Name, appInstanceId));
+		assertFalse(usher.canExecute(username2, task2Name, appInstanceId));
 	}
 }
